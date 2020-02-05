dd7b227bdf0d32234a09b14d9ec7d0082c6e36a7
lwzm <lwzm@qq.com>
2020-02-05 13:17:06 +0800

    clean

--- jupyter/jupyter_notebook_config.py
+++
@@ -1,8 +0,0 @@
-# https://jupyter-notebook.readthedocs.io/en/stable/config.html
-from os import environ
-from notebook.auth import passwd
-c.NotebookApp.ip = '0.0.0.0'
-c.NotebookApp.port = 80
-c.NotebookApp.allow_root = True
-c.NotebookApp.open_browser = False
-c.NotebookApp.password = passwd(environ.get("PASSWORD") or "")
